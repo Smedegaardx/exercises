4aaec9550aef9b53bd46915662402f6bb3d5df21
Anthon <anan0013@stud.kea.dk>
2026-01-27 11:28:04 +0100

    Computer valg startet

--- Sten_Saks_Papir/script.js
+++ Sten_Saks_Papir/script.js
@@ -1,8 +1,5 @@
-const sten = 1;
-const papir = 2;
-const saks = 3;
-
 const player = document.getElementById("player1");
+const computer = document.getElementById("player2");
 
 document.getElementById("rockButton").addEventListener("click", choseRock);
 document.getElementById("paperButton").addEventListener("click", chosePaper);
@@ -12,22 +9,31 @@ function choseRock() {
   player.classList.add("rock");
   player.classList.remove("paper", "scissors");
   animateHands();
+  computerStart();
 }
 function chosePaper() {
   player.classList.add("paper");
   player.classList.remove("rock", "scissors");
   animateHands();
+  computerStart();
 }
 function choseScissors() {
   player.classList.add("scissors");
   player.classList.remove("paper", "rock");
   animateHands();
+  computerStart();
 }
 
 function animateHands() {
   player.classList.add("shake");
+  computer.classList.add("shake");
 
   setTimeout(function () {
     player.classList.remove("shake");
+    computer.classList.remove("shake");
   }, 1850);
+}
+
+function computerStart() {
+  // Computer vælger hånd
 }

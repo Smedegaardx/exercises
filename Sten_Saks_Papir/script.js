58afeba94d1610e2f5258aaceb15df045f7b7e9c
Anthon <anan0013@stud.kea.dk>
2026-01-27 11:22:08 +0100

    valg og animation sat op

--- Sten_Saks_Papir/script.js
+++ Sten_Saks_Papir/script.js
@@ -1,3 +1,33 @@
 const sten = 1;
 const papir = 2;
 const saks = 3;
+
+const player = document.getElementById("player1");
+
+document.getElementById("rockButton").addEventListener("click", choseRock);
+document.getElementById("paperButton").addEventListener("click", chosePaper);
+document.getElementById("scissorsButton").addEventListener("click", choseScissors);
+
+function choseRock() {
+  player.classList.add("rock");
+  player.classList.remove("paper", "scissors");
+  animateHands();
+}
+function chosePaper() {
+  player.classList.add("paper");
+  player.classList.remove("rock", "scissors");
+  animateHands();
+}
+function choseScissors() {
+  player.classList.add("scissors");
+  player.classList.remove("paper", "rock");
+  animateHands();
+}
+
+function animateHands() {
+  player.classList.add("shake");
+
+  setTimeout(function () {
+    player.classList.remove("shake");
+  }, 1850);
+}

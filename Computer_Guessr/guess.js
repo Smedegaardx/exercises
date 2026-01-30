9a198d7664e3d915e96c363c1569b1b3fce948d3
Anthon <anan0013@stud.kea.dk>
2026-01-31 00:04:11 +0100

    Spillets fonkunalitet opsat

--- Computer_Guessr/guess.js
+++ Computer_Guessr/guess.js
@@ -1,5 +1,43 @@
-document.getElementById("start").addEventListener("click", startGame);
+let pcGuess = 50;
+let divider = 50;
+let totalGuesses = 1;
 
-function startGame() {}
+const pcGuessField = document.getElementById("computerGuess");
 
-// Hvis højere plus 50% og round, hvis lavere minus
+document.getElementById("startBtn").addEventListener("click", startGame);
+document.getElementById("lowerBtn").addEventListener("click", lower);
+document.getElementById("higherBtn").addEventListener("click", higher);
+document.getElementById("correctBtn").addEventListener("click", iWin);
+
+function startGame() {
+  pcGuessField.innerHTML = `Jeg gætter på ${pcGuess}`;
+  correctBtn.disabled = false;
+  lowerBtn.disabled = false;
+  higherBtn.disabled = false;
+  startBtn.disabled = true;
+}
+
+function lower() {
+  pcGuess = Math.round(pcGuess - divider / 2);
+  divider /= 2;
+  totalGuesses = totalGuesses + 1;
+  pcGuessField.innerHTML = `Jeg gætter på ${pcGuess}`;
+}
+
+function higher() {
+  pcGuess = Math.round(pcGuess + divider / 2);
+  divider /= 2;
+  totalGuesses = totalGuesses + 1;
+  pcGuessField.innerHTML = `Jeg gætter på ${pcGuess}`;
+}
+
+function iWin() {
+  pcGuessField.innerHTML = `Nemt! Jeg gættede det på ${totalGuesses} gæt!`;
+  pcGuess = 50;
+  divider = 50;
+  totalGuesses = 1;
+  correctBtn.disabled = true;
+  lowerBtn.disabled = true;
+  higherBtn.disabled = true;
+  startBtn.disabled = false;
+}

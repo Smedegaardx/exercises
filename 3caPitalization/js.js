c60aca003b91305581f6a9c565bf85db0c7b16df
Anthon <anan0013@stud.kea.dk>
2026-02-06 13:55:43 +0100

    First Lettter

--- 3caPitalization/js.js
+++ 3caPitalization/js.js
@@ -3,3 +3,8 @@ const peter = "peter";
 const result = peter.substring(0, 2) + peter.substring(2, 3).toUpperCase() + peter.substring(3, 5);
 
 console.log(result);
+
+const RealCapitalization = "løngangstræde";
+
+const result2 = RealCapitalization.substring(0, 1).toUpperCase() + RealCapitalization.substring(1);
+console.log(result2);

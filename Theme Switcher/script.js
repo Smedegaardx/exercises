8328681e965e07c340a5ae2ac9773f3202fc759e
Anthon <anan0013@stud.kea.dk>
2026-02-13 13:53:06 +0100

    Theme switcher updated with localStorage

--- Theme Switcher/script.js
+++ Theme Switcher/script.js
@@ -1,7 +1,14 @@
 const body = document.querySelector("body");
 
+document.addEventListener("DOMContentLoaded", oldTheme);
+
 document.querySelector("#themes").addEventListener("change", swapTheme);
 
 function swapTheme(evt) {
   body.dataset.theme = `${evt.target.value}`;
+  localStorage.setItem("theme", evt.target.value);
+}
+
+function oldTheme() {
+  body.dataset.theme = `${localStorage.theme}`;
 }

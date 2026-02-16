0e666e4baef808287d5fe2211c3ffb7cccba29e1
Smedegaardx <anan0013@stud.kea.dk>
2026-02-17 00:54:48 +0100

    ToDo funkionalitet

--- To_Do/script.js
+++ To_Do/script.js
@@ -1,16 +1,23 @@
 let toDoArray = [];
 const listField = document.querySelector("#listField");
+const doneListField = document.querySelector("#doneListField");
 const addBtn = document.querySelector("#addButton");
 const newTaskText = document.querySelector("#newTaskText");
 const newTaskAmount = document.querySelector("#newTaskAmount");
 const newTaskTime = document.querySelector("#newTaskTime");
-const taskMenu = document.querySelector("svg");
 
 addBtn.addEventListener("click", addTask);
+
+// Tager localstorage og viser listen, efter siden er loadet
+window.addEventListener("DOMContentLoaded", () => {retrievedString = localStorage.getItem("tasks");
+retrievedArray = JSON.parse(retrievedString);
+toDoArray = retrievedArray
+displayTasks()})
 
 function addTask() {
   const toDoObj = { text: newTaskText.value, amount: newTaskAmount.value, time: newTaskTime.value, done: false, id: self.crypto.randomUUID() };
   toDoArray.push(toDoObj);
+  // Reset input fields
   newTaskText.value = "";
   newTaskAmount.value = "";
   newTaskTime.value = "";
@@ -20,23 +27,69 @@ function addTask() {
 
 function displayTasks() {
   listField.innerHTML = "";
-  toDoArray.forEach((item) => {
+  doneListField.innerHTML = "";
+  toDoArray.forEach((item) =>  { if (item.done == false){
     listField.innerHTML += `<li class="listItem">
         <div class="listItemDesc">
-          <input type="checkbox" />
+          <input type="checkbox" class="checkbox" data-id="${item.id}" />
           <p class="listItemText">${item.text}</p>
           <p class="listItemAmount">${item.amount ?? ""}</p>
         </div>
         <div class="listEnd">
           <p class="listTime">${item.time ?? ""}</p>
+          <a class="menuButton">
           <svg viewBox="0,0 100,100" height="40" width="40" role="img" alt="more" class="menuicon">
             <circle cx="50" cy="15" r="10" />
             <circle cx="50" cy="50" r="10" />
             <circle cx="50" cy="85" r="10" />
           </svg>
+          </a>
         </div>
-      </li>`;
+      </li>`;}
+      else {doneListField.innerHTML += `<li class="listItem">
+        <div class="listItemDesc">
+          <input type="checkbox" class="checkbox" checked data-id="${item.id}"/>
+          <p class="listItemText">${item.text}</p>
+          <p class="listItemAmount">${item.amount ?? ""}</p>
+        </div>
+        <div class="listEnd">
+          <p class="listTime">${item.time ?? ""}</p>
+          <a class="menuButton">
+          <svg viewBox="0,0 100,100" height="40" width="40" role="img" alt="more" class="menuicon">
+            <circle cx="50" cy="15" r="10" />
+            <circle cx="50" cy="50" r="10" />
+            <circle cx="50" cy="85" r="10" />
+          </svg>
+          </a>
+        </div>
+      </li>`;}
   });
+
+  // Sæt eventlistener på hver checkbox som bliver skabt fra arrayet
+checkboxes = document.querySelectorAll('.checkbox')
+checkboxes.forEach((box) => {
+  box.addEventListener("click", (evt) => {
+    moveTask(evt)
+  });
+});
+// Sæt eventlistener på hver menuknap som bliver skabt fra arrayet
+buttons = document.querySelectorAll('.menuButton')
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    openMenu()
+  });
+});
+  // Gemmer array i localstorage, ved at gøre det til JSON
+string = JSON.stringify(toDoArray)
+localStorage.setItem("tasks", string)
+}
+
+// Rykker en task fra to-do til done hvis der klikkes på dens checkbox
+function moveTask(evt){
+thisItem = toDoArray.find(item => item.id == evt.target.dataset.id)
+if(thisItem.done == false){ thisItem.done = true}
+else if(thisItem.done == true){thisItem.done = false}
+  displayTasks()
 }
 
 function openMenu() {
